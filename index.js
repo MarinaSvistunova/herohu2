b8d5d278e01e0cb3ea2f1f47aad8a52582216ab9
Свистунова Марина Петровна <31745345+MarinaSvistunova@users.noreply.github.com>
2018-12-20 14:14:22 +0300

    first

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 4321;
+/*const PORT = 4321;
 const express = require('express');
 const app = express();
 app
@@ -7,3 +7,17 @@ app
   .use((e, r, res, n) => res.status(500).end(`Error: $(e)`))
   .set('view engine', 'pug')
   .listen(process.env.PORT || PORT, () => console.log(process.pid));
+*/
+
+const PORT = 3336;
+
+const serve = require('serve-static')('public', {
+  'index': ['index.html', 'index.htm']
+});
+console.log(__dirname);
+require('http').Server((req, res) => {
+  if (req.url === '/hello') res.writeHead(200) || res.end('Hello!');
+  serve(req, res, () => res.writeHead(404) || res.end('not found'));
+})
+
+.listen(PORT, () => console.log(`running ${process.pid} at port ${PORT}`));
